2cd1266657de8bb1d73ea6780afb5059634f5139
keysking <keysking.cn@gmail.com>
2023-05-05 20:39:16 +0800

    校验位功能

--- src/components/SendPanel/SendPanel.vue
+++ src/components/SendPanel/SendPanel.vue
@@ -3,8 +3,9 @@ import { useRecordStore } from "@/store/useRecordStore";
 import { useSerialStore } from "@/store/useSerialStore";
 import { useSettingStore } from "@/store/useSettingStore";
 
+import { useCheckDigit } from "@/utils/useCheckDigit/useCheckDigit";
 import { useDataCode } from "@/utils/useDataCode/useDataCode";
-import { inject, ref, watch } from "vue";
+import { computed, inject, ref, watch } from "vue";
 import AutoSendButton from "./components/AutoSendButton.vue";
 import HexShower from "./components/HexShower.vue";
 
@@ -13,6 +14,7 @@ const { sendHex: bleSendHex } = inject("ble");
 const { records, addRecord } = useRecordStore();
 const { lineEnding, deviceType, sendHexInputMode } = useSettingStore();
 const { sendType } = useSerialStore();
+const { checkAlgorithm, checkAlgorithms } = useCheckDigit();
 const {
   dataCode,
   hexStringToHexFormat,
@@ -21,9 +23,34 @@ const {
   stringToHexFormat,
   stringToHexString,
   isHexString,
+  bufferToHexFormat,
 } = useDataCode();
 
-const sendData = ref("1234AB2B");
+const sendData = ref("");
+
+const sendBuffer = computed(() => {
+  if (sendType.value == "hex") {
+    if (checkDigit.value) {
+      return Uint8Array.from([
+        ...hexStringToBuffer(sendData.value),
+        ...checkDigit.value,
+      ]);
+    }
+    return Uint8Array.from([...hexStringToBuffer(sendData.value)]);
+  } else {
+    return stringToBuffer(sendData.value + lineEnding.value);
+  }
+});
+
+const checkDigit = computed(() => {
+  if (!checkAlgorithm.value || sendType.value != "hex") return undefined;
+  return checkAlgorithm.value.algorithm(hexStringToBuffer(sendData.value));
+});
+
+const checkDigitHexFormat = computed(() => {
+  if (!checkAlgorithm.value) return undefined;
+  return bufferToHexFormat(checkDigit.value);
+});
 
 async function sendHex(data) {
   if (deviceType.value == "serial") {
@@ -34,25 +61,13 @@ async function sendHex(data) {
 }
 
 async function send() {
-  if (sendType.value == "hex") {
-    const data = hexStringToBuffer(sendData.value);
-    await sendHex(data);
-    addRecord({
-      type: "write",
-      data: data,
-      time: new Date(),
-      display: sendType.value,
-    });
-  } else {
-    const data = stringToBuffer(sendData.value + lineEnding.value);
-    await sendHex(data);
-    addRecord({
-      type: "write",
-      data: data,
-      time: new Date(),
-      display: sendType.value,
-    });
-  }
+  await sendHex(sendBuffer.value);
+  addRecord({
+    type: "write",
+    data: sendBuffer.value,
+    time: new Date(),
+    display: sendType.value,
+  });
 }
 watch(sendType, (value) => {
   if (!sendData.value) return;
@@ -92,6 +107,30 @@ function clear() {
       v-model="sendData"
     ></textarea>
     <div class="absolute right-3 bottom-3 flex gap-x-3">
+      <div class="dropdown dropdown-top" v-if="sendType == 'hex'">
+        <label
+          tabindex="0"
+          class="btn"
+          :class="checkAlgorithm ? 'btn-success btn-outline' : 'btn-ghost'"
+        >
+          {{
+            checkAlgorithm
+              ? `${checkAlgorithm.name}(${checkDigitHexFormat})`
+              : "校验位"
+          }}
+        </label>
+        <ul
+          tabindex="0"
+          class="dropdown-content menu p-2 shadow bg-base-200 rounded-box w-28"
+        >
+          <li v-for="item in checkAlgorithms">
+            <a @click="checkAlgorithm = item">{{ item.name }}</a>
+          </li>
+          <li>
+            <a @click="checkAlgorithm = undefined">不校验</a>
+          </li>
+        </ul>
+      </div>
       <AutoSendButton class="" @send="send" />
       <button class="btn px-10" @click="send">发 送</button>
     </div>
