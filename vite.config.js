41f604225f60c69fc46bc3c5a2ef0f5c416f4247
Drswith <drswith@outlook.com>
2025-06-26 20:15:45 +0800

    chore: 添加vue dev tools

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,17 @@
-import vue from "@vitejs/plugin-vue";
+
 import path from "node:path";
 import { defineConfig, loadEnv } from 'vite'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   return {
-    plugins: [vue()],
+    plugins: [
+      vue(),
+      vueDevTools(),
+    ],
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src"),
