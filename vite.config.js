1e361f7f7185e77a7d0c349fa5ff3381cf7b5560
Drswith <drswith@outlook.com>
2025-06-26 20:02:55 +0800

    chore: 添加环境变量支持

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,19 @@
 import vue from "@vitejs/plugin-vue";
-import path from "path";
-import { defineConfig } from "vite";
+import path from "node:path";
+import { defineConfig, loadEnv } from 'vite'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+// https://vite.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-});
+    server: {
+      port: Number(env.VITE_APP_DEV_PORT),
+    },
+  } 
+})
