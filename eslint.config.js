1868b7b08d721ee32fd7c539f37bc7f3d6b5c87f
drswith <540628938@qq.com>
2025-06-28 18:30:49 +0000

    style: 移除jsconfig.json中的空行并调整eslint规则
    调整jsconfig.json文件格式，移除多余空行
    更新eslint配置，关闭部分严格规则并调整警告级别

--- eslint.config.js
+++ eslint.config.js
@@ -10,6 +10,15 @@ export default antfu(
 
     rules: {
       'no-console': 'off',
+      'no-unused-vars': 'off',
+      'eqeqeq': 'warn',
+      'vue/eqeqeq': 'warn',
+      'vue/no-useless-v-bind': 'warn',
+      'unused-imports/no-unused-vars': 'warn',
+      'vue/require-v-for-key': 'warn',
+      'no-restricted-globals': 'off',
+      'no-async-promise-executor': 'off',
+      'no-unmodified-loop-condition': 'warn',
     },
   },
 )
