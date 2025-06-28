3862be44e116fbef3b4af186da4e856cdad2a4ef
drswith <540628938@qq.com>
2025-06-28 18:33:37 +0000

    feat(ActivityBar): 添加活动栏组件包含主题切换和工具菜单
    实现活动栏组件，包含以下功能：
    - GitHub和B站链接按钮
    - 暗黑/明亮主题切换功能
    - 工具下拉菜单（驱动下载、ASCII码表等）
    - 设置按钮

---
+++ src/components/ActivityBar/ActivityBar.vue
@@ -0,0 +1,73 @@
+<script setup>
+import { useDark, useToggle } from '@vueuse/core'
+import { useDialog } from '@/components/Dialog'
+import { Button } from '@/components/ui/button'
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+
+const { open } = useDialog()
+
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
+</script>
+
+<template>
+  <div class="bg-input flex flex-col p-2">
+    <a href="https://github.com/BaudDance/SerialAssistant" target="_blank">
+      <Button as-child variant="ghost" size="icon" class="w-12 h-12 p-3">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><!-- Icon from Material Design Icons by Pictogrammers - https://github.com/Templarian/MaterialDesign/blob/master/LICENSE --><path fill="currentColor" d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5c.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34c-.46-1.16-1.11-1.47-1.11-1.47c-.91-.62.07-.6.07-.6c1 .07 1.53 1.03 1.53 1.03c.87 1.52 2.34 1.07 2.91.83c.09-.65.35-1.09.63-1.34c-2.22-.25-4.55-1.11-4.55-4.92c0-1.11.38-2 1.03-2.71c-.1-.25-.45-1.29.1-2.64c0 0 .84-.27 2.75 1.02c.79-.22 1.65-.33 2.5-.33s1.71.11 2.5.33c1.91-1.29 2.75-1.02 2.75-1.02c.55 1.35.2 2.39.1 2.64c.65.71 1.03 1.6 1.03 2.71c0 3.82-2.34 4.66-4.57 4.91c.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0 0 12 2" /></svg>
+      </Button>
+    </a>
+    <a href="https://space.bilibili.com/6100925" target="_blank">
+      <Button as-child variant="ghost" size="icon" class="w-12 h-12 p-3">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><!-- Icon from Tabler Icons by Paweł Kuna - https://github.com/tabler/tabler-icons/blob/master/LICENSE --><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 10a4 4 0 0 1 4-4h10a4 4 0 0 1 4 4v6a4 4 0 0 1-4 4H7a4 4 0 0 1-4-4zm5-7l2 3m6-3l-2 3m-5 7v-2m6 0v2" /></svg>
+      </Button>
+    </a>
+    <div class="flex-1" />
+
+    <Button as-child variant="ghost" size="icon" class="w-12 h-12 p-3 cursor-pointer" @click="() => toggleDark()">
+      <svg v-if="isDark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><!-- Icon from Material Symbols by Google - https://github.com/google/material-design-icons/blob/master/LICENSE --><path fill="currentColor" d="M12 21q-3.75 0-6.375-2.625T3 12t2.625-6.375T12 3q.35 0 .688.025t.662.075q-1.025.725-1.638 1.888T11.1 7.5q0 2.25 1.575 3.825T16.5 12.9q1.375 0 2.525-.613T20.9 10.65q.05.325.075.662T21 12q0 3.75-2.625 6.375T12 21" /></svg>
+      <svg v-else xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><!-- Icon from Material Symbols by Google - https://github.com/google/material-design-icons/blob/master/LICENSE --><path fill="currentColor" d="M12 17q-2.075 0-3.537-1.463T7 12t1.463-3.537T12 7t3.538 1.463T17 12t-1.463 3.538T12 17m-7-4H1v-2h4zm18 0h-4v-2h4zM11 5V1h2v4zm0 18v-4h2v4zM6.4 7.75L3.875 5.325L5.3 3.85l2.4 2.5zm12.3 12.4l-2.425-2.525L17.6 16.25l2.525 2.425zM16.25 6.4l2.425-2.525L20.15 5.3l-2.5 2.4zM3.85 18.7l2.525-2.425L7.75 17.6l-2.425 2.525z" /></svg>
+    </Button>
+
+    <DropdownMenu>
+      <DropdownMenuTrigger as-child>
+        <Button as-child variant="ghost" size="icon" class="w-12 h-12 cursor-pointer  p-3">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!-- Icon from Game Icons by GameIcons - https://github.com/game-icons/icons/blob/master/license.txt --><path fill="currentColor" d="M32 96v64h448V96zm0 128v64h448v-64zm0 128v64h448v-64z" /></svg>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent class="w-56 select-none" side="right">
+        <DropdownMenuLabel>小工具</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem class="cursor-pointer" @select="open('downLoadDriver')">
+            <span>
+              常见驱动下载
+            </span>
+          </DropdownMenuItem>
+          <DropdownMenuItem class="cursor-pointer" @select="open('ascii')">
+            <span>
+              ASCII码表
+            </span>
+          </DropdownMenuItem>
+          <a href="https://led.baud-dance.com/" target="_blank">
+            <DropdownMenuItem class="cursor-pointer">
+              LED字模生成器
+            </DropdownMenuItem>
+          </a>
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+    <Button as-child variant="ghost" size="icon" class="w-12 h-12 p-3 cursor-pointer" @click="open('setting')">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><!-- Icon from Material Symbols by Google - https://github.com/google/material-design-icons/blob/master/LICENSE --><path fill="currentColor" d="m9.25 22l-.4-3.2q-.325-.125-.612-.3t-.563-.375L4.7 19.375l-2.75-4.75l2.575-1.95Q4.5 12.5 4.5 12.338v-.675q0-.163.025-.338L1.95 9.375l2.75-4.75l2.975 1.25q.275-.2.575-.375t.6-.3l.4-3.2h5.5l.4 3.2q.325.125.613.3t.562.375l2.975-1.25l2.75 4.75l-2.575 1.95q.025.175.025.338v.674q0 .163-.05.338l2.575 1.95l-2.75 4.75l-2.95-1.25q-.275.2-.575.375t-.6.3l-.4 3.2zm2.8-6.5q1.45 0 2.475-1.025T15.55 12t-1.025-2.475T12.05 8.5q-1.475 0-2.488 1.025T8.55 12t1.013 2.475T12.05 15.5" /></svg>
+    </Button>
+  </div>
+</template>
