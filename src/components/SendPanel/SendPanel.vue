81087080d86543a5044080689dcf7e849dd27cf1
drswith <540628938@qq.com>
2025-06-28 18:36:01 +0000

    refactor(SendPanel): 重构发送面板组件，提取逻辑到store
    将发送面板的复杂逻辑提取到useSendStore中，简化组件代码
    使用新的UI组件库替换原有实现，改进自动发送按钮交互

--- src/components/SendPanel/SendPanel.vue
+++ src/components/SendPanel/SendPanel.vue
@@ -1,181 +1,64 @@
 <script setup>
-import { useRecordStore } from "@/store/useRecordStore";
-import { useSerialStore } from "@/store/useSerialStore";
-import { useSettingStore } from "@/store/useSettingStore";
-import { useMagicKeys } from "@vueuse/core";
+import { useSendStore } from '@/store/useSendStore'
+import AutoSendButton from './components/AutoSendButton.vue'
 
-import { useCheckDigit } from "@/utils/useCheckDigit/useCheckDigit";
-import { useDataCode } from "@/utils/useDataCode/useDataCode";
-import { computed, inject, ref, watch } from "vue";
-import AutoSendButton from "./components/AutoSendButton.vue";
-
-const { sendHex: serialSendHex } = inject("serial");
-const { sendHex: bleSendHex } = inject("ble");
-const { records, addRecord } = useRecordStore();
-const { lineEnding, deviceType, sendHexInputMode } = useSettingStore();
-const { sendType } = useSerialStore();
-const { checkAlgorithm, checkAlgorithms } = useCheckDigit();
-const {
-  decStringToBuffer,
-  hexStringToBuffer,
-  stringToBuffer,
-  stringToHexString,
-  isHexString,
-  bufferToHexFormat,
-} = useDataCode();
-
-const sendData = ref("");
-
-const sendBuffer = computed(() => {
-  if (sendType.value == "hex") {
-    if (checkDigit.value) {
-      return Uint8Array.from([
-        ...hexStringToBuffer(sendData.value.replaceAll(" ", "")),
-        ...checkDigit.value,
-      ]);
-    }
-    return hexStringToBuffer(sendData.value.replaceAll(" ", ""));
-  } else if (sendType.value == "ascii") {
-    console.log("ascii", stringToBuffer(sendData.value + lineEnding.value));
-    return stringToBuffer(sendData.value + lineEnding.value);
-  } else {
-    console.log("dec", decStringToBuffer(sendData.value.replaceAll(" ", "")));
-    return decStringToBuffer(sendData.value.replaceAll(" ", ""))
-  }
-});
-
-const checkDigit = computed(() => {
-  if (!checkAlgorithm.value || sendType.value != "hex") return undefined;
-  if (sendData.value.length == 0) return [0x00];
-  return checkAlgorithm.value.algorithm(
-    hexStringToBuffer(sendData.value.replaceAll(" ", ""))
-  );
-});
-
-const checkDigitHexFormat = computed(() => {
-  if (!checkAlgorithm.value) return undefined;
-  return bufferToHexFormat(checkDigit.value);
-});
-
-async function sendHex(data) {
-  if (deviceType.value == "serial") {
-    await serialSendHex(data);
-  } else {
-    await bleSendHex(data);
-  }
-}
-
-async function send() {
-  await sendHex(sendBuffer.value);
-  addRecord({
-    type: "write",
-    data: sendBuffer.value,
-    time: new Date(),
-    display: sendType.value,
-  });
-}
-watch(sendType, (value) => {
-  if (!sendData.value) return;
-  if (value == "hex") {
-    // 若为普通字符串,先取编码,然后转换为hex格式
-    if (!isHexString(sendData.value)) {
-      sendData.value = stringToHexString(sendData.value);
-    }
-  }
-});
-
-function onInput() {
-  if (sendType.value == "hex") {
-    sendData.value = sendData.value
-      .replace(/([^0-9a-fA-F ])/, "")
-      .toUpperCase();
-  }
-  else if (sendType.value == "dec") {
-    sendData.value = sendData.value
-      .replace(/([^0-9])/, "")
-  }
-}
-
-function clear() {
-  sendData.value = "";
-}
-
-const { ctrl_s } = useMagicKeys({
-  passive: false,
-  onEventFired(e) {
-    if (e.ctrlKey && e.key === "s" && e.type === "keydown") e.preventDefault();
-  },
-});
-watch(ctrl_s, (v) => {
-  if (v) reformat();
-});
-function reformat() {
-  if (sendType.value == "hex") {
-    reformatHex();
-  }
-}
-
-function reformatHex() {
-  if (sendData.value.length == 0) return;
-  let data = sendData.value;
-  // 首先使用空格分割 并去掉空字符串
-  data = data.split(" ").filter((item) => item);
-  // 在长度为奇数的字符串的最后一位前补0
-  data = data.map((item) => {
-    if (item.length % 2 == 1) {
-      // 最后一位前补0
-      return item.slice(0, item.length - 1) + "0" + item.slice(item.length - 1);
-    }
-    return item;
-  });
-  // 将数组转换为字符串
-  data = data.join("");
-  // 将字符串按照每两个字符分割
-  data = data.match(/.{1,2}/g);
-  // 将数组转换为字符串
-  data = data.join(" ");
-  sendData.value = data;
-}
+const { sendData, clear, onInput, send, sendType, checkAlgorithm, checkAlgorithms, checkDigitHexFormat, reformat } = useSendStore()
 </script>
 
 <template>
-  <div class="relative">
-    <textarea id="send-panel-input" type="text-area" class="w-full h-full rounded-xl px-3 bg-transparent pb-10 pr-40"
-      autocomplete="send-panel-input" @input="onInput" v-model="sendData"></textarea>
-    <div class="absolute right-3 bottom-3 flex gap-x-3">
-      <div class="dropdown dropdown-top" v-if="sendType == 'hex'">
-        <label tabindex="0" class="btn" :class="checkAlgorithm ? 'btn-success btn-outline' : 'btn-ghost'">
-          {{
-            checkAlgorithm
-            ? `${checkAlgorithm.name}(${checkDigitHexFormat})`
-            : "校验位"
-          }}
-        </label>
-        <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-200 rounded-box w-28">
-          <li v-for="item in checkAlgorithms">
-            <a @click="checkAlgorithm = item">{{ item.name }}</a>
-          </li>
-          <li>
-            <a @click="checkAlgorithm = undefined">不校验</a>
-          </li>
-        </ul>
-      </div>
-      <AutoSendButton class="" @send="send" />
-      <button class="btn px-10" :class="sendData.length == 0 ? 'btn-disabled' : ''" @click="send">
-        发 送
-      </button>
-    </div>
-    <div class="absolute right-3 top-2 flex flex-col gap-y-2">
-      <div class="tooltip" data-tip="清空输入框">
-        <button class="btn btn-ghost btn-circle btn-sm" @click="clear">
-          <img class="w-5 h-5" src="/clear_context.svg" />
-        </button>
-      </div>
-      <div class="tooltip" data-tip="ctrl+s">
-        <button v-if="sendType == 'hex'" class="btn btn-ghost btn-circle btn-sm lowercase" @click="reformat">
-          <span class="text-xs">整理</span>
-        </button>
-      </div>
+  <div class="flex flex-col h-full">
+    <textarea
+      id="send-panel-input"
+      v-model="sendData"
+      type="text-area"
+      class="flex-1 w-full bg-transparent focus:outline-none focus-visible:outline-none text-sm pt-1 px-3 pb-3"
+      autocomplete="send-panel-input"
+      @input="onInput"
+    />
+
+    <div class="p-3  flex justify-end space-x-2">
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger>
+            <Button as-child variant="ghost" class="w-9 h-9 cursor-pointer p-1.5" @click="reformat">
+              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><!-- Icon from Material Symbols by Google - https://github.com/google/material-design-icons/blob/master/LICENSE --><path fill="currentColor" d="M11 22q-.825 0-1.412-.587T9 20v-4H6q-.825 0-1.412-.587T4 14V7q0-1.65 1.175-2.825T8 3h12v11q0 .825-.587 1.413T18 16h-3v4q0 .825-.587 1.413T13 22zM6 10h12V5h-1v4h-2V5h-1v2h-2V5H8q-.825 0-1.412.588T6 7zm0 4h12v-2H6zm0 0v-2z" /></svg>
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>点击整理发送格式</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+
+      <DropdownMenu>
+        <DropdownMenuTrigger as-child class="cursor-pointer">
+          <Button variant="ghost">
+            {{
+              checkAlgorithm
+                ? `${checkAlgorithm}(${checkDigitHexFormat})`
+                : "校验位"
+            }}
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent side="top">
+          <DropdownMenuLabel>末尾添加校验位</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuRadioGroup v-model="checkAlgorithm">
+            <DropdownMenuRadioItem v-for="(item, index) in checkAlgorithms" :key="index" :value="item.name">
+              {{ item.name }}
+            </DropdownMenuRadioItem>
+            <DropdownMenuRadioItem value="">
+              不校验
+            </DropdownMenuRadioItem>
+          </DropdownMenuRadioGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      <AutoSendButton />
+
+      <Button>
+        发送
+      </Button>
     </div>
   </div>
 </template>
--- src/components/SendPanel/components/HexShower.vue
+++ src/components/SendPanel/components/HexShower.vue
@@ -1,98 +1,102 @@
 <script setup>
-import { useSettingStore } from "@/store/useSettingStore";
-import { nextTick, ref, watch } from "vue";
+import { nextTick, ref, watch } from 'vue'
+import { useSettingStore } from '@/store/useSettingStore'
 
-const props = defineProps(["modelValue"]);
-const emit = defineEmits(["update:modelValue"]);
-const { sendHexInputMode } = useSettingStore();
+const props = defineProps(['modelValue'])
+const emit = defineEmits(['update:modelValue'])
+const { sendHexInputMode } = useSettingStore()
 
 const groups = ref(
   props.modelValue.length == 0
-    ? [""]
-    : props.modelValue.replaceAll(" ", "").match(/.{1,2}/g)
-);
-const items = ref([]);
+    ? ['']
+    : props.modelValue.replaceAll(' ', '').match(/.{1,2}/g),
+)
+const items = ref([])
 watch(
   [sendHexInputMode],
   () => {
-    if (sendHexInputMode.value == "format") return;
-    groups.value =
-      props.modelValue.length == 0
-        ? [""]
-        : props.modelValue.replaceAll(" ", "").match(/.{1,2}/g);
+    if (sendHexInputMode.value == 'format')
+      return
+    groups.value
+      = props.modelValue.length == 0
+        ? ['']
+        : props.modelValue.replaceAll(' ', '').match(/.{1,2}/g)
   },
-  { deep: true }
-);
+  { deep: true },
+)
 watch(
   groups,
   (n) => {
     // 不足两位的前面补0
     const data = groups.value
-      .map((v) => v.padStart(2, "0"))
-      .join("")
-      .replace(/(.{2})/g, "$1 ")
-      .trim();
-    emit("update:modelValue", data);
+      .map(v => v.padStart(2, '0'))
+      .join('')
+      .replace(/(.{2})/g, '$1 ')
+      .trim()
+    emit('update:modelValue', data)
     if (n.length == 0) {
-      groups.value = [""];
+      groups.value = ['']
     }
   },
-  { deep: true }
-);
+  { deep: true },
+)
 async function onInput(index, e) {
-  const v = e.target.value.replace(/([^0-9a-fA-F])/, "").toUpperCase();
-  e.target.value = v;
+  const v = e.target.value.replace(/([^0-9a-f])/i, '').toUpperCase()
+  e.target.value = v
   if (v.length == 0 && index > 0) {
-    groups.value.splice(index, 1);
-    await nextTick();
-    items.value[index - 1].getElementsByTagName("input")[0].focus();
-  } else if (v.length <= 2) {
-    groups.value[index] = v;
-  } else if (v.length > 2) {
+    groups.value.splice(index, 1)
+    await nextTick()
+    items.value[index - 1].getElementsByTagName('input')[0].focus()
+  }
+  else if (v.length <= 2) {
+    groups.value[index] = v
+  }
+  else if (v.length > 2) {
     // 每两个字符为一组,加入groups,然后坐标移到下一个input
-    groups.value[index] = v.slice(0, 2);
+    groups.value[index] = v.slice(0, 2)
     for (let i = 2; i < v.length; i += 2) {
-      groups.value.splice(index + i / 2, 0, v.slice(i, i + 2));
+      groups.value.splice(index + i / 2, 0, v.slice(i, i + 2))
     }
-    await nextTick();
+    await nextTick()
     items.value[index + Math.ceil(v.length / 2) - 1]
-      .getElementsByTagName("input")[0]
-      .focus();
+      .getElementsByTagName('input')[0]
+      .focus()
   }
 }
 async function onBlur(index, e) {
-  const v = e.target.value;
+  const v = e.target.value
   if (v.length == 0 && groups.value.length > 1) {
-    groups.value.splice(index, 1);
+    groups.value.splice(index, 1)
   }
 }
 async function onDeleteDown(index, e) {
-  const v = e.target.value;
+  const v = e.target.value
   if (
-    (e.key === "Backspace" || e.key === "Delete") &&
-    v.length == 0 &&
-    index > 0
+    (e.key === 'Backspace' || e.key === 'Delete')
+    && v.length == 0
+    && index > 0
   ) {
-    groups.value.splice(index, 1);
-    await nextTick();
-    items.value[index - 1].getElementsByTagName("input")[0].focus();
+    groups.value.splice(index, 1)
+    await nextTick()
+    items.value[index - 1].getElementsByTagName('input')[0].focus()
   }
 }
 </script>
+
 <template>
   <div class="flex flex-wrap gap-y-1 content-start">
     <span v-for="(g, i) in groups" ref="items">
       <label>0x</label>
       <input
-        :id="'hexinput' + i"
-        :ref="'hex' + i"
-        :value="g"
+        :id="`hexinput${i}`"
+        :ref="`hex${i}`"
         :key="i"
+        :value="g"
         class="w-6 bg-transparent"
         autocomplete="off"
         @input="(e) => onInput(i, e)"
         @blur="(e) => onBlur(i, e)"
-      />
+      >
       <span v-if="i != groups.length - 1">,&nbsp;</span>
     </span>
   </div>
