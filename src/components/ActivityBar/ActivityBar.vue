9aed140d51d8f6ee4aca4ad50e3e8fe41204bbb5
drswith <540628938@qq.com>
2025-06-28 18:48:35 +0000

    fix(ActivityBar): 隐藏B站链接按钮
    由于暂时不需要展示B站链接，添加v-if条件将其隐藏

--- src/components/ActivityBar/ActivityBar.vue
+++ src/components/ActivityBar/ActivityBar.vue
@@ -26,11 +26,13 @@ const toggleDark = useToggle(isDark)
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><!-- Icon from Material Design Icons by Pictogrammers - https://github.com/Templarian/MaterialDesign/blob/master/LICENSE --><path fill="currentColor" d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5c.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34c-.46-1.16-1.11-1.47-1.11-1.47c-.91-.62.07-.6.07-.6c1 .07 1.53 1.03 1.53 1.03c.87 1.52 2.34 1.07 2.91.83c.09-.65.35-1.09.63-1.34c-2.22-.25-4.55-1.11-4.55-4.92c0-1.11.38-2 1.03-2.71c-.1-.25-.45-1.29.1-2.64c0 0 .84-.27 2.75 1.02c.79-.22 1.65-.33 2.5-.33s1.71.11 2.5.33c1.91-1.29 2.75-1.02 2.75-1.02c.55 1.35.2 2.39.1 2.64c.65.71 1.03 1.6 1.03 2.71c0 3.82-2.34 4.66-4.57 4.91c.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0 0 12 2" /></svg>
       </Button>
     </a>
-    <a href="https://space.bilibili.com/6100925" target="_blank">
+
+    <a v-if="false" href="https://space.bilibili.com/6100925" target="_blank">
       <Button as-child variant="ghost" size="icon" class="w-12 h-12 p-3">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><!-- Icon from Tabler Icons by Paweł Kuna - https://github.com/tabler/tabler-icons/blob/master/LICENSE --><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 10a4 4 0 0 1 4-4h10a4 4 0 0 1 4 4v6a4 4 0 0 1-4 4H7a4 4 0 0 1-4-4zm5-7l2 3m6-3l-2 3m-5 7v-2m6 0v2" /></svg>
       </Button>
     </a>
+
     <div class="flex-1" />
 
     <Button as-child variant="ghost" size="icon" class="w-12 h-12 p-3 cursor-pointer" @click="() => toggleDark()">
