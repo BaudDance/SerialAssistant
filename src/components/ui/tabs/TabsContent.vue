a21928b1909fbffcc221600721e5f1c57df302cd
drswith <540628938@qq.com>
2025-06-28 18:31:47 +0000

    feat(ui): 新增多个UI组件及工具函数
    添加Switch、Checkbox、Textarea、Separator、ScrollArea、Table、Tabs、Resizable、Popover、Tooltip、Sheet等组件
    为每个组件创建对应的Vue文件和导出文件
    添加工具函数valueUpdater和badgeVariants

---
+++ src/components/ui/tabs/TabsContent.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { reactiveOmit } from "@vueuse/core";
+import { TabsContent } from "reka-ui";
+import { cn } from "@/lib/utils";
+
+const props = defineProps({
+  value: { type: [String, Number], required: true },
+  forceMount: { type: Boolean, required: false },
+  asChild: { type: Boolean, required: false },
+  as: { type: null, required: false },
+  class: { type: null, required: false },
+});
+
+const delegatedProps = reactiveOmit(props, "class");
+</script>
+
+<template>
+  <TabsContent
+    data-slot="tabs-content"
+    :class="cn('flex-1 outline-none', props.class)"
+    v-bind="delegatedProps"
+  >
+    <slot />
+  </TabsContent>
+</template>
