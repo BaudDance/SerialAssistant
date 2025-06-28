571bd877cd106df017a8d19d3d29b3030754e197
drswith <540628938@qq.com>
2025-06-28 18:33:58 +0000

    feat(Dialog): 新增ASCII码表和设置对话框组件
    添加ASCII码表对话框组件，显示完整的ASCII字符集及其二进制、十进制和十六进制表示
    新增设置对话框组件，包含行尾添加、中文编码和十进制格式等配置选项

--- src/components/Dialog/index.js
+++ src/components/Dialog/index.js
@@ -1,3 +1,4 @@
+export { default as AsciiDialog } from './Ascii.vue'
 export { dialogKeys, useDialog } from './composable'
-
 export { default as DownLoadDriverDialog } from './DownLoadDriver.vue'
+export { default as SettingDialog } from './Setting.vue'
