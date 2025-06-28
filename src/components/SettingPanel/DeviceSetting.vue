505a6a4ed6f3d72b809652311dac6897fb0ad3fa
drswith <540628938@qq.com>
2025-06-28 18:35:48 +0000

    feat(设备设置): 重构设备设置界面并使用新UI组件
    - 将串口和蓝牙设置界面改为标签页布局
    - 使用新的Select组件替换原生select元素
    - 统一按钮样式并使用新的Button组件
    - 添加设置项的描述文本和标题
    - 优化自定义波特率的处理逻辑

--- src/components/SettingPanel/DeviceSetting.vue
+++ src/components/SettingPanel/DeviceSetting.vue
@@ -1,5 +1,5 @@
 <script setup>
-import Bar from '@/components/SettingPanel/components/DeviceSetting/Bar.vue'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import { useSettingStore } from '@/store/useSettingStore.js'
 import BleSetting from './components/DeviceSetting/BleSetting.vue'
 import SerialSetting from './components/DeviceSetting/SerialSetting.vue'
@@ -8,10 +8,22 @@ const { deviceType } = useSettingStore()
 </script>
 
 <template>
-  <div class="flex flex-col">
-    <SerialSetting v-if="deviceType === 'serial'" />
-    <BleSetting v-else />
-    <div class="flex-1" />
-    <Bar />
-  </div>
+  <Tabs v-model="deviceType" class="p-2">
+    <TabsList class="grid w-full grid-cols-2">
+      <TabsTrigger value="serial">
+        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><!-- Icon from Material Design Icons by Pictogrammers - https://github.com/Templarian/MaterialDesign/blob/master/LICENSE --><path fill="currentColor" d="M7 3h10v2h2v3h-3v6H8V8H5V5h2zm10 6h2v5h-2zm-6 6h2v7h-2zM5 9h2v5H5z" /></svg>
+        串口
+      </TabsTrigger>
+      <TabsTrigger value="ble">
+        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><!-- Icon from Material Design Icons by Pictogrammers - https://github.com/Templarian/MaterialDesign/blob/master/LICENSE --><path fill="currentColor" d="M14.88 16.29L13 18.17v-3.76m0-8.58l1.88 1.88L13 9.58m4.71-1.87L12 2h-1v7.58L6.41 5L5 6.41L10.59 12L5 17.58L6.41 19L11 14.41V22h1l5.71-5.71l-4.3-4.29z" /></svg>
+        蓝牙
+      </TabsTrigger>
+    </TabsList>
+    <TabsContent value="serial">
+      <SerialSetting />
+    </TabsContent>
+    <TabsContent value="ble">
+      <BleSetting />
+    </TabsContent>
+  </Tabs>
 </template>
