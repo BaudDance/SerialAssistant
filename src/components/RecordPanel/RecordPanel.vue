4f64ef74f1f846583ed81a9502a51f11dce985c8
drswith <540628938@qq.com>
2025-06-28 18:35:32 +0000

    refactor(RecordPanel): 优化代码格式和组件结构
    - 重新组织导入语句顺序
    - 统一使用单引号
    - 简化模板中的空div标签
    - 调整文本大小从text-lg到text-md

--- src/components/RecordPanel/RecordPanel.vue
+++ src/components/RecordPanel/RecordPanel.vue
@@ -1,35 +1,36 @@
 <script setup>
-import { useRecordStore } from "@/store/useRecordStore";
-import { useDataCode } from "@/utils/useDataCode/useDataCode";
-import { refThrottled, useScroll } from "@vueuse/core";
-import { format } from "date-fns";
-import { computed, ref, watch } from "vue";
-import { useSerialStore } from "@/store/useSerialStore";
-const { bufferToDecFormat, bufferToHexFormat, bufferToString, stringToHtml } = useDataCode();
-const { records, readingRecord, pinBottom } = useRecordStore();
-const { recordTypes } = useSerialStore();
-const rootEl = ref(null);
+import { refThrottled, useScroll } from '@vueuse/core'
+import { format } from 'date-fns'
+import { computed, ref, watch } from 'vue'
+import { useRecordStore } from '@/store/useRecordStore'
+import { useSerialStore } from '@/store/useSerialStore'
+import { useDataCode } from '@/utils/useDataCode/useDataCode'
+
+const { bufferToDecFormat, bufferToHexFormat, bufferToString, stringToHtml } = useDataCode()
+const { records, readingRecord, pinBottom } = useRecordStore()
+const { recordTypes } = useSerialStore()
+const rootEl = ref(null)
 const { x, y, isScrolling, arrivedState, directions } = useScroll(rootEl, {
-  behavior: "smooth",
-});
+  behavior: 'smooth',
+})
 async function scrollToBottom() {
-  rootEl.value.scrollTop = rootEl.value.scrollHeight + 2000;
+  rootEl.value.scrollTop = rootEl.value.scrollHeight + 2000
 }
 
 async function toggoleRecordDisplay(record) {
-  const index = recordTypes.value.indexOf(record.display);
-  record.display = recordTypes.value[(index + 1) % recordTypes.value.length];
+  const index = recordTypes.value.indexOf(record.display)
+  record.display = recordTypes.value[(index + 1) % recordTypes.value.length]
 }
-const recordLength = computed(() => records.value.length);
+const recordLength = computed(() => records.value.length)
 watch(
   [recordLength, refThrottled(readingRecord, 150)],
   (value, oldValue) => {
     if (pinBottom.value & (value[0] != oldValue[0])) {
-      setTimeout(() => scrollToBottom(), 0);
+      setTimeout(() => scrollToBottom(), 0)
     }
   },
-  { deep: true }
-);
+  { deep: true },
+)
 </script>
 
 <template>
@@ -41,16 +42,16 @@ watch(
           <div class="text-sm opacity-70">
             {{ format(record.time, "HH:mm:ss:SSS") }}
           </div>
-          <div class="w-4"></div>
+          <div class="w-4" />
           <div class="cursor-pointer" @click="() => toggoleRecordDisplay(record)">
             {{ record.display }}
           </div>
         </div>
-        <div class="chat-bubble break-words text-lg">
+        <div class="chat-bubble break-words text-md">
           <div v-if="record.display == 'hex'">
             {{ bufferToHexFormat(record.data) }}
           </div>
-          <div v-else-if="record.display == 'ascii'" v-html="stringToHtml(bufferToString(record.data))"></div>
+          <div v-else-if="record.display == 'ascii'" v-html="stringToHtml(bufferToString(record.data))" />
           <div v-else>
             {{ bufferToDecFormat(record.data) }}
           </div>
@@ -68,34 +69,34 @@ watch(
           <div class="text-sm opacity-70">
             {{ format(record.time, "HH:mm:ss:SSS") }}
           </div>
-          <div class="w-4"></div>
+          <div class="w-4" />
           <div class="cursor-pointer" @click="() => toggoleRecordDisplay(record)">
             {{ record.display }}
           </div>
         </div>
-        <div class="chat-bubble break-words text-lg">
+        <div class="chat-bubble break-words text-md">
           <div v-if="record.display == 'hex'">
             {{ bufferToHexFormat(record.data) }}
           </div>
-          <div v-else-if="record.display == 'ascii'" v-html="stringToHtml(bufferToString(record.data))"></div>
+          <div v-else-if="record.display == 'ascii'" v-html="stringToHtml(bufferToString(record.data))" />
           <div v-else>
             {{ bufferToDecFormat(record.data) }}
           </div>
         </div>
       </div>
     </template>
-    <div class="chat chat-start" v-if="readingRecord">
+    <div v-if="readingRecord" class="chat chat-start">
       <div class="chat-header mx-2">
         <!-- TODO 点击可以切换时间显示格式(是否显示日期) -->
         <div class="text-sm opacity-70">
           {{ format(readingRecord.time, "HH:mm:ss:SSS") }}
         </div>
       </div>
-      <div class="chat-bubble break-all text-lg">
+      <div class="chat-bubble break-all text-md">
         <div v-if="readingRecord.display == 'hex'">
           {{ bufferToHexFormat(readingRecord.data) }}
         </div>
-        <div v-else v-html="stringToHtml(bufferToString(readingRecord.data))"></div>
+        <div v-else v-html="stringToHtml(bufferToString(readingRecord.data))" />
       </div>
     </div>
   </div>
