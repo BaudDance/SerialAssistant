0923b62f6f6cf18c08a299f685435a0537a2644a
drswith <540628938@qq.com>
2025-06-29 07:45:25 +0000

    feat(布局): 优化响应式布局并添加移动端支持
    重构 ActivityBar 和主布局结构，使用 flex-row 在移动端和 flex-col 在桌面端
    添加 breakpoints 检测实现响应式布局切换
    引入 SerialRateDialog 组件

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script setup>
+import { breakpointsTailwind, useBreakpoints } from '@vueuse/core'
 import { defineAsyncComponent, provide } from 'vue'
-import { ActivityBar, StatusBar } from '@/components'
+
+import ActivityBar from '@/components/ActivityBar/ActivityBar.vue'
 import ControlPanel from '@/components/ControlPanel/ControlPanel.vue'
 import RecordPanel from '@/components/RecordPanel/RecordPanel.vue'
 import SendPanel from '@/components/SendPanel/SendPanel.vue'
 import DeviceSetting from '@/components/SettingPanel/DeviceSetting.vue'
+import StatusBar from '@/components/StatusBar/StatusBar.vue'
 
 import {
   ResizableHandle,
@@ -21,11 +24,16 @@ import { useSerialStore } from '@/store/useSerialStore'
 const AsciiDialog = defineAsyncComponent(() => import('@/components/Dialog/Ascii.vue'))
 const DownLoadDriverDialog = defineAsyncComponent(() => import('@/components/Dialog/DownLoadDriver.vue'))
 const SettingDialog = defineAsyncComponent(() => import('@/components/Dialog/Setting.vue'))
+const SerialRateDialog = defineAsyncComponent(() => import('@/components/Dialog/SerialRate.vue'))
 
 const { readingRecord, addRecord } = useRecordStore()
 const { readType } = useSerialStore()
 
 listenNetworkStatus()
+
+const breakpoints = useBreakpoints(breakpointsTailwind)
+
+const lgAndLarger = breakpoints.greaterOrEqual('lg') // lg and larger
 
 function onReadData(data) {
   if (readingRecord.value) {
@@ -66,9 +74,12 @@ provide('ble', ble)
 
 <template>
   <div class="flex justify-center items-center">
-    <div class="container 2xl:mx-56 aspect-video h-screen py-[40px] lg:py-[100px] flex flex-col-reverse  lg:flex-row relative">
-      <ActivityBar class="rounded-l-lg border-t border-l border-b" />
+    <div class="container 2xl:mx-56 aspect-video h-screen py-[40px] lg:py-[100px] flex flex-col lg:flex-row relative">
+      <ActivityBar
+        class="rounded-t-lg rounded-b-none border-r border-l border-b lg:rounded-l-lg lg:rounded-r-none lg:border-t lg:border-l lg:border-b"
+      />
       <ResizablePanelGroup
+        v-if="lgAndLarger"
         direction="horizontal"
         class="rounded-r-lg border-t border-r border-b"
       >
@@ -94,26 +105,41 @@ provide('ble', ble)
           </ResizablePanelGroup>
         </ResizablePanel>
       </ResizablePanelGroup>
-      <!-- <SiderBar />
-      <DeviceSetting class="lg:basis-1/4 border-solid border-2 border-gray-400  p-5" />
-      <div class="w-7" />
-      <div class="flex flex-col flex-1 lg:basis-3/4 space-y-2">
-        <RecordPanel class="flex-1 lg:basis-3/4 border-solid border-2 border-gray-400 " />
-        <ControlPanel />
-        <SendPanel class="lg:basis-1/4 border-solid border-2 border-gray-400 " />
-      </div>
 
-      <div class="text-sm m-2 absolute bottom-[15px] lg:bottom-[75px] right-0">
+      <ResizablePanelGroup v-else direction="vertical" class="rounded-b-lg border-l border-r border-b">
+        <ResizablePanel :default-size="70">
+          <ResizablePanelGroup
+            direction="horizontal"
+          >
+            <ResizablePanel :default-size="20">
+              <div class=" h-full bg-card ">
+                <DeviceSetting />
+              </div>
+            </ResizablePanel>
+            <ResizableHandle with-handle />
+            <ResizablePanel :default-size="80">
+              <RecordPanel class="bg-card w-full h-full" />
+            </ResizablePanel>
+          </ResizablePanelGroup>
+        </ResizablePanel>
+        <ResizableHandle with-handle />
+        <ResizablePanel :default-size="30">
+          <div class="flex flex-col bg-card h-full">
+            <ControlPanel class="" />
+            <SendPanel class="flex-1" />
+            <StatusBar class="bg-input" />
+          </div>
+        </ResizablePanel>
+      </ResizablePanelGroup>
+
+      <!-- <div class="text-sm m-2 absolute bottom-[15px] lg:bottom-[75px] right-0">
         powered by 波特律动
-      </div>
-      <UserNumberFotter class="absolute  bottom-[15px] lg:bottom-[75px] left-0 m-2" /> -->
+      </div> -->
     </div>
   </div>
-  <!-- <DownloadDriverModal />
-  <SettingModal />
-  <ASCIIModel />
-  <SerialRateModal /> -->
+
   <AsciiDialog />
   <DownLoadDriverDialog />
   <SettingDialog />
+  <SerialRateDialog />
 </template>
