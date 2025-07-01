c8da3666b819ca21b41b8b80168cf223d364084f
drswith <540628938@qq.com>
2025-07-01 08:12:54 +0000

    refactor(layout): 重构布局管理逻辑并添加顶部控制栏
    - 将独立的全屏功能整合到统一的布局管理 composable 中
    - 新增顶部控制栏组件用于集中管理布局状态
    - 优化响应式布局逻辑，自动处理不同屏幕尺寸下的布局切换
    - 添加设置面板和发送面板的折叠/展开功能

---
+++ src/components/TopBar/TopBar.vue
@@ -0,0 +1,60 @@
+<script  setup>
+import { useLayout } from '@/composables/useLayout'
+
+const { showFullScreen, showSettingPanel, settingPanelPosition, showSendPanel, smallerThanLg } = useLayout()
+</script>
+
+<template>
+  <div class="py-1 px-2 flex items-center justify-end">
+    <div class="flex items-center space-x-1">
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger>
+            <Toggle v-model="showSettingPanel" class="min-w-0 w-6 h-6 p-0 cursor-pointer">
+              <template v-if="settingPanelPosition === 'left'">
+                <svg v-if="showSettingPanel" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><!-- Icon from Tabler Icons by Paweł Kuna - https://github.com/tabler/tabler-icons/blob/master/LICENSE --><path fill="currentColor" d="M6 21a3 3 0 0 1-3-3V6a3 3 0 0 1 3-3h12a3 3 0 0 1 3 3v12a3 3 0 0 1-3 3zM18 5h-8v14h8a1 1 0 0 0 1-1V6a1 1 0 0 0-1-1" /></svg>
+                <svg v-else xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><!-- Icon from Tabler Icons by Paweł Kuna - https://github.com/tabler/tabler-icons/blob/master/LICENSE --><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2zm5-2v16" /></svg>
+              </template>
+
+              <template v-else>
+                <svg v-if="showSettingPanel" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><!-- Icon from Tabler Icons by Paweł Kuna - https://github.com/tabler/tabler-icons/blob/master/LICENSE --><path fill="currentColor" d="M6 21a3 3 0 0 1-3-3V6a3 3 0 0 1 3-3h12a3 3 0 0 1 3 3v12a3 3 0 0 1-3 3zm8-16H6a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8z" /></svg>
+                <svg v-else xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><!-- Icon from Tabler Icons by Paweł Kuna - https://github.com/tabler/tabler-icons/blob/master/LICENSE --><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2zm11-2v16" /></svg>
+              </template>
+            </Toggle>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>点击{{ showSettingPanel ? '折叠' : '展开' }}设备设置栏</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger>
+            <Toggle v-model="showSendPanel" class="min-w-0 w-6 h-6 p-0 cursor-pointer">
+              <svg v-if="showSendPanel" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><!-- Icon from Tabler Icons by Paweł Kuna - https://github.com/tabler/tabler-icons/blob/master/LICENSE --><path fill="currentColor" d="M18 3a3 3 0 0 1 2.995 2.824L21 6v12a3 3 0 0 1-2.824 2.995L18 21H6a3 3 0 0 1-2.995-2.824L3 18V6a3 3 0 0 1 2.824-2.995L6 3zm0 2H6a1 1 0 0 0-.993.883L5 6v9h14V6a1 1 0 0 0-.883-.993z" /></svg>
+              <svg v-else xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><!-- Icon from Tabler Icons by Paweł Kuna - https://github.com/tabler/tabler-icons/blob/master/LICENSE --><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2zm0 9h16" /></svg>
+            </Toggle>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>点击{{ showSendPanel ? '折叠' : '展开' }}输入面板</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+
+      <TooltipProvider v-if="!smallerThanLg">
+        <Tooltip>
+          <TooltipTrigger>
+            <Toggle v-model="showFullScreen" class="min-w-0 w-6 h-6 p-0 cursor-pointer">
+              <svg v-if="showFullScreen" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><!-- Icon from Material Symbols by Google - https://github.com/google/material-design-icons/blob/master/LICENSE --><path fill="currentColor" d="M6 21v-3H3v-2h5v5zm10 0v-5h5v2h-3v3zM3 8V6h3V3h2v5zm13 0V3h2v3h3v2z" /></svg>
+              <svg v-else xmlns="http://www.w3.org/2000/svg" style="width: 16px; height: 16px;" viewBox="0 0 24 24"><!-- Icon from Material Symbols by Google - https://github.com/google/material-design-icons/blob/master/LICENSE --><path fill="currentColor" d="M3 21v-5h2v3h3v2zm13 0v-2h3v-3h2v5zM3 8V3h5v2H5v3zm16 0V5h-3V3h5v5z" /></svg>
+            </Toggle>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>点击{{ showFullScreen ? '退出全尺寸' : '全尺寸' }}显示工作窗口</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+    </div>
+  </div>
+</template>
---
+++ src/composables/useLayout/index.js
@@ -0,0 +1,117 @@
+import { breakpointsTailwind, createGlobalState, useBreakpoints, useStorage } from '@vueuse/core'
+import { ref } from 'vue'
+
+export const DEFAULT_LAYOUT_CONFIG = Object.freeze({
+  showFullScreen: false,
+  showSettingPanel: true,
+  showTopBar: true,
+  showActivityBar: true,
+  showSendPanel: true,
+})
+
+/**
+ * 布局管理 Composable
+ * 提供全局布局状态管理和持久化存储
+ */
+export const useLayout = createGlobalState(() => {
+  // 响应式断点
+  const breakpoints = useBreakpoints(breakpointsTailwind)
+  const smallerThanLg = breakpoints.smaller('lg') // 小于 lg(1024px)
+
+  // 全屏状态
+  const showFullScreen = useStorage('layout:showFullScreen', ref(DEFAULT_LAYOUT_CONFIG.showFullScreen))
+  const toggleFullScreen = () => {
+    showFullScreen.value = !showFullScreen.value
+  }
+
+  // 设置面板
+  const showSettingPanel = useStorage('layout:showSettingPanel', ref(DEFAULT_LAYOUT_CONFIG.showSettingPanel))
+  const toggleSettingPanel = () => {
+    showSettingPanel.value = !showSettingPanel.value
+  }
+
+  // 顶部栏 - 原则上不建议隐藏，但提供接口供用户自定义
+  const showTopBar = useStorage('layout:showTopBar', ref(DEFAULT_LAYOUT_CONFIG.showTopBar))
+  const toggleTopBar = () => {
+    showTopBar.value = !showTopBar.value
+  }
+
+  // 活动栏 - 原则上不建议隐藏，但提供接口供用户自定义
+  const showActivityBar = useStorage('layout:showActivityBar', ref(DEFAULT_LAYOUT_CONFIG.showActivityBar))
+  const toggleActivityBar = () => {
+    showActivityBar.value = !showActivityBar.value
+  }
+
+  // 发送栏
+  const showSendPanel = useStorage('layout:showSendPanel', ref(DEFAULT_LAYOUT_CONFIG.showSendPanel))
+  const toggleSendPanel = () => {
+    showSendPanel.value = !showSendPanel.value
+  }
+
+  // 批量更新配置 - 使用函数式编程思想，让每个配置项自己处理更新逻辑
+  const updateLayoutConfig = (config) => {
+    // 定义更新器函数映射 - 只需要在这里添加新的配置项
+    const updaters = {
+      showFullScreen: value => showFullScreen.value = value,
+      showSettingPanel: value => showSettingPanel.value = value,
+      showTopBar: value => showTopBar.value = value,
+      showActivityBar: value => showActivityBar.value = value,
+      showSendPanel: value => showSendPanel.value = value,
+    }
+
+    Object.entries(config).forEach(([key, value]) => {
+      if (value !== undefined && updaters[key]) {
+        updaters[key](value)
+      }
+      else if (value !== undefined) {
+        console.warn(`Unknown layout config key: ${key}`)
+      }
+    })
+  }
+
+  // 重置为默认配置
+  const resetLayoutConfig = () => {
+    updateLayoutConfig(DEFAULT_LAYOUT_CONFIG)
+  }
+
+  // 获取当前完整配置
+  const getCurrentConfig = () => ({
+    showFullScreen: showFullScreen.value,
+    showSettingPanel: showSettingPanel.value,
+    showTopBar: showTopBar.value,
+    showActivityBar: showActivityBar.value,
+    showSendPanel: showSendPanel.value,
+  })
+
+  return {
+    // 响应式断点
+    smallerThanLg,
+
+    // 状态
+    showFullScreen,
+    showSettingPanel,
+    showTopBar,
+    showActivityBar,
+    showSendPanel,
+
+    // 全屏控制
+    toggleFullScreen,
+
+    // 设置面板控制
+    toggleSettingPanel,
+
+    // 顶部栏控制
+    toggleTopBar,
+
+    // 活动栏控制
+    toggleActivityBar,
+
+    // 发送栏控制
+    toggleSendPanel,
+
+    // 批量操作
+    updateLayoutConfig,
+    resetLayoutConfig,
+    getCurrentConfig,
+  }
+})
