74a3535eb5248a47d73b22e221c78ca4259cf55e
drswith <540628938@qq.com>
2025-07-12 13:30:32 +0000

    feat(useCheckDigit): 添加奇偶校验、异或校验和ModbusCRC16算法
    添加三种新的校验算法实现并包含完整的单元测试

--- src/composables/useCheckDigit/useCheckDigit.js
+++ src/composables/useCheckDigit/useCheckDigit.js
@@ -10,6 +10,41 @@ const checkAlgorithms = ref([
       return Uint8Array.from([data.reduce((a, b) => a + b, 0) & 0xFF])
     },
   },
+  {
+    name: '奇偶校验',
+    algorithm: (data) => {
+      console.log('奇偶校验', data)
+      const parity = data.reduce((a, b) => a + b, 0) % 2
+      return Uint8Array.from([parity])
+    },
+  },
+  {
+    name: '异或校验',
+    algorithm: (data) => {
+      console.log('异或校验', data)
+      return Uint8Array.from([data.reduce((a, b) => a ^ b, 0)])
+    },
+  },
+  {
+    name: 'ModbusCRC16',
+    algorithm: (data) => {
+      console.log('ModbusCRC16', data)
+      let crc = 0xFFFF
+      for (let i = 0; i < data.length; i++) {
+        crc ^= data[i]
+        for (let j = 0; j < 8; j++) {
+          if (crc & 0x01) {
+            crc >>= 1
+            crc ^= 0xA001
+          }
+          else {
+            crc >>= 1
+          }
+        }
+      }
+      return Uint8Array.from([crc & 0xFF, crc >> 8])
+    },
+  },
 ])
 
 export function useCheckDigit() {
