6e486b74f1c8ac2ed20a712f24d5640c1b6b4d8b
drswith <540628938@qq.com>
2025-07-24 07:41:41 +0000

    build: 添加vitest路径别名配置以简化模块导入

--- vitest.config.js
+++ vitest.config.js
@@ -1,8 +1,14 @@
+import path from 'node:path'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
     include: ['test/**/*.test.js'],
     environment: 'jsdom',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
 })
