5871257f7b452568285f204ad23edfb227f0e38f
drswith <540628938@qq.com>
2025-07-26 16:06:27 +0000

    feat(设置对话框): 重构设置界面为标签页布局并添加缓存设置
    将设置对话框重构为标签页布局，分为常规设置和缓存设置两个标签页
    添加新的缓存设置组件 RecordCache
    调整对话框宽度和样式以适配新布局

--- src/components/Dialog/Setting.vue
+++ src/components/Dialog/Setting.vue
@@ -12,67 +12,84 @@ const { hasDecTyps } = useSerialStore()
 
 <template>
   <Dialog v-model:open="visible[dialogKeys.setting]">
-    <DialogContent class="">
-      <DialogHeader class="">
+    <DialogContent class="max-w-lg">
+      <DialogHeader>
         <DialogTitle>设置</DialogTitle>
         <DialogDescription>
-          常用设置
+          应用程序配置
         </DialogDescription>
       </DialogHeader>
 
-      <div class="flex h-9">
-        <Label class="w-36">行尾自动添加</Label>
-        <Select v-model="lineEndingMode">
-          <SelectTrigger class="w-full">
-            <SelectValue placeholder="请选择结束符" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectGroup>
-              <SelectItem value="无">
-                无
-              </SelectItem>
-              <SelectItem value="LF">
-                LF
-              </SelectItem>
-              <SelectItem value="CR">
-                CR
-              </SelectItem>
-              <SelectItem value="CRLF">
-                CRLF
-              </SelectItem>
-              <SelectItem value="自定义">
-                自定义
-              </SelectItem>
-            </SelectGroup>
-          </SelectContent>
-        </Select>
-      </div>
+      <Tabs default-value="general" class="w-full">
+        <TabsList class="grid w-full grid-cols-2">
+          <TabsTrigger value="general">
+            常规设置
+          </TabsTrigger>
+          <TabsTrigger value="cache">
+            缓存设置
+          </TabsTrigger>
+        </TabsList>
 
-      <div class="flex h-9">
-        <Label class="w-36">中文编码</Label>
-        <Select v-model="dataCode">
-          <SelectTrigger class="w-full">
-            <SelectValue placeholder="请选择中文编码方式" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectGroup>
-              <SelectItem value="UTF-8">
-                UTF-8
-              </SelectItem>
-              <SelectItem value="GBK">
-                GBK
-              </SelectItem>
-            </SelectGroup>
-          </SelectContent>
-        </Select>
-      </div>
-      <div class="flex h-9">
-        <Label class="w-36">十进制格式</Label>
-        <div class="w-full flex items-center">
-          <Checkbox v-model="hasDecTyps" />
-        </div>
-      </div>
-      <DialogFooter />
+        <TabsContent value="general" class="space-y-4 mt-4">
+          <div class="flex h-9">
+            <Label class="w-36">行尾自动添加</Label>
+            <Select v-model="lineEndingMode">
+              <SelectTrigger class="w-full">
+                <SelectValue placeholder="请选择结束符" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectItem value="无">
+                    无
+                  </SelectItem>
+                  <SelectItem value="LF">
+                    LF
+                  </SelectItem>
+                  <SelectItem value="CR">
+                    CR
+                  </SelectItem>
+                  <SelectItem value="CRLF">
+                    CRLF
+                  </SelectItem>
+                  <SelectItem value="自定义">
+                    自定义
+                  </SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
+
+          <div class="flex h-9">
+            <Label class="w-36">中文编码</Label>
+            <Select v-model="dataCode">
+              <SelectTrigger class="w-full">
+                <SelectValue placeholder="请选择中文编码方式" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectItem value="UTF-8">
+                    UTF-8
+                  </SelectItem>
+                  <SelectItem value="GBK">
+                    GBK
+                  </SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
+
+          <div class="flex h-9">
+            <Label class="w-36">十进制格式</Label>
+            <div class="w-full flex items-center">
+              <Checkbox v-model="hasDecTyps" />
+            </div>
+          </div>
+        </TabsContent>
+
+        <TabsContent value="cache" class="space-y-6 mt-4">
+          <RecordCache />
+        </TabsContent>
+      </Tabs>
     </DialogContent>
   </Dialog>
 </template>
