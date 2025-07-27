bdbad8d68d67280da8cf189f72ae1ec8576a53ea
drswith <540628938@qq.com>
2025-07-27 19:34:48 +0000

    refactor(types): 更新类型声明文件中的组件和导入路径
    移除不再使用的RecordCache组件声明
    添加RouterLink和RouterView的类型声明
    统一useRecordCache的导入路径为index.js

--- src/auto-imports.d.ts
+++ src/auto-imports.d.ts
@@ -224,7 +224,7 @@ declare global {
   const usePreferredReducedMotion: typeof import('@vueuse/core')['usePreferredReducedMotion']
   const usePrevious: typeof import('@vueuse/core')['usePrevious']
   const useRafFn: typeof import('@vueuse/core')['useRafFn']
-  const useRecordCache: typeof import('./composables/useRecordCache/useRecordCache.js')['useRecordCache']
+  const useRecordCache: typeof import('./composables/useRecordCache/index.js')['useRecordCache']
   const useRefHistory: typeof import('@vueuse/core')['useRefHistory']
   const useResizeObserver: typeof import('@vueuse/core')['useResizeObserver']
   const useScreenOrientation: typeof import('@vueuse/core')['useScreenOrientation']
@@ -524,7 +524,7 @@ declare module 'vue' {
     readonly usePreferredReducedMotion: UnwrapRef<typeof import('@vueuse/core')['usePreferredReducedMotion']>
     readonly usePrevious: UnwrapRef<typeof import('@vueuse/core')['usePrevious']>
     readonly useRafFn: UnwrapRef<typeof import('@vueuse/core')['useRafFn']>
-    readonly useRecordCache: UnwrapRef<typeof import('./composables/useRecordCache/useRecordCache.js')['useRecordCache']>
+    readonly useRecordCache: UnwrapRef<typeof import('./composables/useRecordCache/index.js')['useRecordCache']>
     readonly useRefHistory: UnwrapRef<typeof import('@vueuse/core')['useRefHistory']>
     readonly useResizeObserver: UnwrapRef<typeof import('@vueuse/core')['useResizeObserver']>
     readonly useScreenOrientation: UnwrapRef<typeof import('@vueuse/core')['useScreenOrientation']>
