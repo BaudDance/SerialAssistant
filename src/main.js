a5c2a0ad1eabbdee568648878f924f06b07f917f
drswith <540628938@qq.com>
2025-07-27 19:34:55 +0000

    feat(路由): 添加路由支持到主应用
    将路由模块集成到Vue应用中，以便支持页面导航功能

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { router } from './router'
 import './style.css'
-
 // register service worker
 
 // async function registerServiceWorker() {
@@ -45,5 +45,4 @@ if ('serviceWorker' in navigator) {
 // registerServiceWorker();
 
 // mount the main app
-
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
