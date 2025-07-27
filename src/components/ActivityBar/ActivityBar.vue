286c2a9b25b49d6ceaef111a7aad1b7e858e9a75
drswith <540628938@qq.com>
2025-07-27 19:33:32 +0000

    feat(ActivityBar): 添加会话历史导航及设备连接状态检查
    refactor(Setting): 移除设置对话框中的标签页结构

--- src/components/ActivityBar/ActivityBar.vue
+++ src/components/ActivityBar/ActivityBar.vue
@@ -1,7 +1,8 @@
 <script setup>
 import { useDark, useToggle } from '@vueuse/core'
+import { computed, inject, ref } from 'vue'
+import { useRouter } from 'vue-router'
 import { useDialog } from '@/components/Dialog'
-import { Button } from '@/components/ui/button'
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -15,6 +16,17 @@ import {
 import { useLayout } from '@/composables/useLayout'
 
 const { open } = useDialog()
+const router = useRouter()
+
+// 注入串口和蓝牙连接状态
+const serial = inject('serial')
+const ble = inject('ble')
+
+// 计算设备是否已连接
+const isConnected = computed(() => serial.connected.value || ble.connected.value)
+
+// Alert Dialog 状态
+const showRecordAlert = ref(false)
 
 const isDark = useDark()
 const toggleDark = useToggle(isDark)
@@ -26,6 +38,30 @@ const buttonClass = computed(() => {
   }
   return 'w-12 h-12 p-3'
 })
+
+// 处理会话历史按钮点击
+function handleRecordClick() {
+  if (isConnected.value) {
+    // 如果有设备连接，显示确认对话框
+    showRecordAlert.value = true
+  }
+  else {
+    // 如果没有设备连接，直接导航
+    router.push('/record')
+  }
+}
+
+// 确认在新标签页打开会话历史
+function confirmOpenRecord() {
+  const url = router.resolve('/record').href
+  window.open(url, '_blank')
+  showRecordAlert.value = false
+}
+
+// 取消操作
+function cancelOpenRecord() {
+  showRecordAlert.value = false
+}
 </script>
 
 <template>
@@ -46,6 +82,11 @@ const buttonClass = computed(() => {
       </a>
     </div>
     <div class="flex flex-col items-center space-y-1">
+      <!-- 会话历史 -->
+      <Button as-child variant="ghost" size="icon" class="cursor-pointer" :class="buttonClass" @click="handleRecordClick">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><!-- Icon from Material Symbols by Google - https://github.com/google/material-design-icons/blob/master/LICENSE --><path fill="currentColor" d="M12 21q-3.15 0-5.575-1.912T3.275 14.2q-.1-.375.15-.687t.675-.363q.4-.05.725.15t.45.6q.6 2.25 2.475 3.675T12 19q2.925 0 4.963-2.037T19 12t-2.037-4.962T12 5q-1.725 0-3.225.8T6.25 8H8q.425 0 .713.288T9 9t-.288.713T8 10H4q-.425 0-.712-.288T3 9V5q0-.425.288-.712T4 4t.713.288T5 5v1.35q1.275-1.6 3.113-2.475T12 3q1.875 0 3.513.713t2.85 1.924t1.925 2.85T21 12t-.712 3.513t-1.925 2.85t-2.85 1.925T12 21m1-9.4l2.5 2.5q.275.275.275.7t-.275.7t-.7.275t-.7-.275l-2.8-2.8q-.15-.15-.225-.337T11 11.975V8q0-.425.288-.712T12 7t.713.288T13 8z" /></svg>
+      </Button>
+
       <!-- 暗色/亮色 -->
       <Button as-child variant="ghost" size="icon" class="cursor-pointer" :class="[buttonClass]" @click="() => toggleDark()">
         <svg v-if="isDark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><!-- Icon from Material Symbols by Google - https://github.com/google/material-design-icons/blob/master/LICENSE --><path fill="currentColor" d="M12 21q-3.75 0-6.375-2.625T3 12t2.625-6.375T12 3q.35 0 .688.025t.662.075q-1.025.725-1.638 1.888T11.1 7.5q0 2.25 1.575 3.825T16.5 12.9q1.375 0 2.525-.613T20.9 10.65q.05.325.075.662T21 12q0 3.75-2.625 6.375T12 21" /></svg>
@@ -88,5 +129,25 @@ const buttonClass = computed(() => {
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><!-- Icon from Material Symbols by Google - https://github.com/google/material-design-icons/blob/master/LICENSE --><path fill="currentColor" d="m9.25 22l-.4-3.2q-.325-.125-.612-.3t-.563-.375L4.7 19.375l-2.75-4.75l2.575-1.95Q4.5 12.5 4.5 12.338v-.675q0-.163.025-.338L1.95 9.375l2.75-4.75l2.975 1.25q.275-.2.575-.375t.6-.3l.4-3.2h5.5l.4 3.2q.325.125.613.3t.562.375l2.975-1.25l2.75 4.75l-2.575 1.95q.025.175.025.338v.674q0 .163-.05.338l2.575 1.95l-2.75 4.75l-2.95-1.25q-.275.2-.575.375t-.6.3l-.4 3.2zm2.8-6.5q1.45 0 2.475-1.025T15.55 12t-1.025-2.475T12.05 8.5q-1.475 0-2.488 1.025T8.55 12t1.013 2.475T12.05 15.5" /></svg>
       </Button>
     </div>
+
+    <!-- Alert Dialog for Record Navigation -->
+    <AlertDialog v-model:open="showRecordAlert">
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>设备已连接</AlertDialogTitle>
+          <AlertDialogDescription>
+            检测到设备已连接，继续前往会话历史将在新标签页打开，以避免中断当前连接。是否继续？
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel class="cursor-pointer" @click="cancelOpenRecord">
+            取消
+          </AlertDialogCancel>
+          <AlertDialogAction class="cursor-pointer" @click="confirmOpenRecord">
+            继续
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   </div>
 </template>
