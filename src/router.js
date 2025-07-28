3ec01b8b151198a5f018ca861ac47fe7d143fca2
drswith <540628938@qq.com>
2025-07-28 01:54:29 +0000

    feat(router): 添加路由导航进度条功能
    使用 useNprogress 组合式 API 在路由切换时显示进度条

--- src/router.js
+++ src/router.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
+import { useNprogress } from '@/composables/useNprogress'
 import Home from '@/pages/index.vue'
 import RecordPage from '@/pages/record.vue'
+
+const NProgress = useNprogress()
 
 // 定义路由
 const router = createRouter({
@@ -11,6 +14,15 @@ const router = createRouter({
     { path: '/404', component: () => import('@/pages/not-found.vue') },
     { path: '/:catchAll(.*)', redirect: '/404' },
   ],
+})
+
+router.beforeEach((_to, _from, next) => {
+  NProgress.start()
+  next()
+})
+
+router.afterEach(() => {
+  NProgress.done()
 })
 
 export { router }
