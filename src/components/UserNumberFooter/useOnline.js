aa8a111711fa941aaa7d5116cdf1fb0155858a8c
drswith <540628938@qq.com>
2025-08-17 11:25:15 +0000

    refactor(utils): 将generateUUID函数提取到utils/uuid模块
    集中管理UUID生成功能，避免代码重复

--- src/components/UserNumberFooter/useOnline.js
+++ src/components/UserNumberFooter/useOnline.js
@@ -3,20 +3,8 @@ import {
   useIntervalFn,
   useLocalStorage,
 } from '@vueuse/core'
-
 import { ref } from 'vue'
-
-/**
- * 生成一个随机的 v4 uuid
- * @returns uuid
- */
-function generateUUID() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
-    const r = Math.random() * 16 | 0
-    const v = c === 'x' ? r : (r & 0x3 | 0x8)
-    return v.toString(16)
-  })
-}
+import { generateUUID } from '@/utils/uuid'
 
 const baseURL = 'https://led.baud-dance.com/api'
 
---
+++ src/utils/uuid.js
@@ -0,0 +1,11 @@
+/**
+ * 生成一个随机的 v4 uuid
+ * @returns uuid
+ */
+export function generateUUID() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+    const r = Math.random() * 16 | 0
+    const v = c === 'x' ? r : (r & 0x3 | 0x8)
+    return v.toString(16)
+  })
+}
